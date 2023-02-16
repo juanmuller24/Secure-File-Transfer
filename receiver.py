0e2cd0e00f3763c1429ac70927f795a1da40f6d9
juanmuller <juan.muller2015@gmail.com>
2023-02-16 10:38:46 -0600

    juan muller 8

--- receiver.py
+++ receiver.py
@@ -24,34 +24,55 @@ def start_server():
     client_socket, client_address = server_socket.accept()
     print('Accepted connection from {}:{}'.format(*client_address))
 
+    mode = client_socket.recv(1024)
+    encryption_type = int.from_bytes(mode, byteorder="big")
+    print(f'the mode is : {encryption_type}')
+
     key = client_socket.recv(1024)
-    print(f'the key on server side ISSS: {key}')
+    print(f'the key is : {key}')
+
+    bits = client_socket.recv(1024)
+    bits_len = int.from_bytes(bits, byteorder="big")
+    print(f'the length of received: {bits}')
+    print(f'the length of the bit: {bits_len}')
 
     with open('received_file.txt', 'wb') as f:
-        while True:
-            # Receive the data in small chunks and retransmit it
 
-            data = client_socket.recv(1024)
+        data = client_socket.recv(bits_len)
 
-            if not data:
-                break
-            f.write(data)
+        if int(encryption_type) == 1:
+            chacha = ChaCha20Poly1305(key)
+            ct = chacha.decrypt(nonce, data, aad)
+            with open('decrypted_file.txt', 'wb') as decrypted_file:
+                decrypted_file.write(ct)
+        elif int(encryption_type) == 2:
+            aesgcm = AESGCM(key)
+            ct = aesgcm.decrypt(nonce, data, aad)
+            with open('decrypted_file.txt', 'wb') as decrypted_file:
+                decrypted_file.write(ct)
+        elif int(encryption_type) == 3:
+            aes0cb3 = AESOCB3(key)
+            ct = aes0cb3.decrypt(nonce, data, aad)
+            with open('decrypted_file.txt', 'wb') as decrypted_file:
+                decrypted_file.write(ct)
+        elif int(encryption_type) == 4:
+            aessiv = AESSIV(key)
+            ct = aessiv.decrypt(data, aad)
+            with open('decrypted_file.txt', 'wb') as decrypted_file:
+                decrypted_file.write(ct)
+        elif int(encryption_type) == 5:
+            aesccm = AESCCM(key)
+            ct = aesccm.decrypt(nonce, data, aad)
+            with open('decrypted_file.txt', 'wb') as decrypted_file:
+                decrypted_file.write(ct)
 
-            # message = b'Hey Juan'
-            # client_socket.sendall(message)
-            # print('Sent: {!r}'.format(message))
+        f.write(data)
+
+        # message = b'Hey Juan'
+        # client_socket.sendall(message)
+        # print('Sent: {!r}'.format(message))
 
     # Clean up the connection
-    with open('received_file.txt', 'rb') as file:
-        data = file.read()
-
-    chacha = ChaCha20Poly1305(key)
-
-    print(f'data: {data}')
-
-    ct = chacha.decrypt(nonce, data, aad)
-    with open('decrypted_file.txt', 'wb') as decrypted_file:
-        decrypted_file.write(ct)
     client_socket.close()
     print('Connection closed')
 
--- sender.py
+++ sender.py
@@ -1,8 +1,6 @@
 import socket
-import time
-
 import encypt
-from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305, AESGCM, AESOCB3, AESSIV, AESCCM
+import time
 
 val = 0
 nonce = val.to_bytes(12, "big")
@@ -11,14 +9,19 @@ enc = encypt.Encrypt()
 
 
 def start_client(mode):
+    global key, encmode, enctype
+
     # Create a TCP/IP socket
-    global key, encmode
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect the socket to the server's address and port
     server_address = (socket.gethostbyname(socket.gethostname()), 12346)
     client_socket.connect(server_address)
     print('Connected to {}:{}'.format(*server_address))
+
+    # sending the type of enc
+    encryption_type = mode.to_bytes()
+    client_socket.send(encryption_type)
 
     # to encrypt data
     if mode == 1:
@@ -27,34 +30,41 @@ def start_client(mode):
 
     elif mode == 2:
         encmode, key = enc.aesgcm()
+        client_socket.send(key)
 
     elif mode == 3:
         encmode, key = enc.aes0cb3()
+        client_socket.send(key)
 
     elif mode == 4:
         encmode, key = enc.aessiv()
+        client_socket.send(key)
 
     elif mode == 5:
         encmode, key = enc.AESCCM()
+        client_socket.send(key)
 
     with open('test_file_1.txt', 'rb') as f:
-        data = f.read(1024)
+        data = f.read()
 
-        while data:
-            if mode == 1 or mode == 2 or mode == 3 or mode == 5:
-                time.sleep(1)
-                encdata = encmode.encrypt(nonce, data, aad)
-                time.sleep(1)
-                client_socket.sendall(encdata)
-                time.sleep(1)
-                print(encdata)
-                data = f.read(1024)
-            else:
-                encdata = encmode.encrypt(data, [b"CS645/745 Modern Cryptography"])
-                client_socket.sendall(encdata)
-                client_socket.send(key)
-                data = f.read(1024)
-            # Send the data
+        # while data:
+        if mode == 1 or mode == 2 or mode == 3 or mode == 5:
+
+            encrypted_data = encmode.encrypt(nonce, data, aad)
+            bit_length = len(encrypted_data)
+            bits = bit_length.to_bytes(12, 'big')
+            print(f'the length of the encrypted data: {bit_length}')
+            print(f'the length of in bytes: {bits}')
+            client_socket.send(bits)
+            time.sleep(2)
+            client_socket.sendall(encrypted_data)
+
+        else:
+            encdata = encmode.encrypt(data, [b"CS645/745 Modern Cryptography"])
+            client_socket.sendall(encdata)
+            client_socket.send(key)
+
+        # Send the data
 
         # Receive the response
         # data = client_socket.recv(1024)
